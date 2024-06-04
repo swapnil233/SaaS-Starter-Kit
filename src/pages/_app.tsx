467be85355963bd146f90de36f74f3614a9551a2
Hasan Iqbal <swapniliqbal@gmail.com>
2024-06-04 16:24:08 -0400

    feat: add Mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import "@mantine/core/styles.css";
+
 import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
