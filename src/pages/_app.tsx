77e21c7c4d187c5ecc96237555f26c45110a9d91
Hasan Iqbal <swapniliqbal@gmail.com>
2024-06-04 17:18:50 -0400

    feat: add roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,10 @@
 import "@mantine/core/styles.css";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin-ext"],
+  weight: ["400", "700"],
+});
 
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
@@ -6,7 +12,9 @@ import { MantineProvider } from "@mantine/core";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider>
-      <Component {...pageProps} />
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
     </MantineProvider>
   );
 }
