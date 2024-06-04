1156090ddb988da3aefd2b9b02328db6b8e59f6e
Hasan Iqbal <swapniliqbal@gmail.com>
2024-06-04 14:37:34 -0400

    init

---
+++ src/pages/api/status.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== "GET") {
+        return res.status(405).json({ message: "Method Not Allowed" });
+
+    }
+
+    return res.status(200).json({ status: "API is working" });
+}
