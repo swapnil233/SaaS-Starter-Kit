131349e52d91ae9376718b909f4aec477208a7b3
Hasan Iqbal <swapniliqbal@gmail.com>
2024-06-10 14:50:16 -0400

    feat: implement user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,22 @@
+import prisma from "@/lib/prisma";
+import { User } from "@prisma/client";
+
+export async function getUserById(id: string): Promise<User> {
+  try {
+    return await prisma.user.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    throw new Error("Error retrieving user by ID in getUserById service");
+  }
+}
+
+export async function getAllUsers(): Promise<User[]> {
+  try {
+    return await prisma.user.findMany();
+  } catch (error) {
+    throw new Error("Error retrieving all users in getAllUsers service");
+  }
+}
