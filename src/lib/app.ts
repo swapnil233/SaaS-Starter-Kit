da63b6d634372ed94b2152574eb14e8deddad6b4
Hasan Iqbal <swapniliqbal@gmail.com>
2024-06-12 01:20:13 -0400

    style: npm run prettier

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -4,7 +4,7 @@ const app = {
   version: packageInfo.version,
   name: "Boilerplate",
   logoUrl: "/google-logo.png",
-  logoUrlAlt: "Placeholder logo from Google. Change this with brand logo."
+  logoUrlAlt: "Placeholder logo from Google. Change this with brand logo.",
 };
 
 export default app;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,55 +1,58 @@
-import { notifications } from '@mantine/notifications';
-import { signIn } from 'next-auth/react';
+import { notifications } from "@mantine/notifications";
+import { signIn } from "next-auth/react";
 
-export const registerUser = async (values: any, setLoading: (_loading: boolean) => void) => {
-    setLoading(true);
+export const registerUser = async (
+  values: any,
+  setLoading: (_loading: boolean) => void
+) => {
+  setLoading(true);
 
-    try {
-        const response = await fetch("/api/auth/register", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(values),
-        });
+  try {
+    const response = await fetch("/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
 
-        // Handle registration error
-        if (!response.ok) {
-            console.error(response);
-            notifications.show({
-                title: "Registration failed",
-                message: `We couldn't register your account. Please refresh the page and try again. If the issue persists, try different credentials.`,
-                color: "red",
-            });
-            setLoading(false);
-            return;
-        }
-
-        // Login user
-        const result = await signIn("credentials", {
-            redirect: true,
-            callbackUrl: "/",
-            email: values.email,
-            password: values.password,
-        });
-
-        if (result?.error) {
-            notifications.show({
-                title: "Login failed",
-                message: `We couldn't log you in. Please try again.`,
-                color: "red",
-            });
-            setLoading(false);
-            return;
-        }
-    } catch (error) {
-        console.error(error);
-        notifications.show({
-            title: "Registration failed",
-            message: `An unexpected error occurred. Please try again later.`,
-            color: "red",
-        });
-    } finally {
-        setLoading(false);
+    // Handle registration error
+    if (!response.ok) {
+      console.error(response);
+      notifications.show({
+        title: "Registration failed",
+        message: `We couldn't register your account. Please refresh the page and try again. If the issue persists, try different credentials.`,
+        color: "red",
+      });
+      setLoading(false);
+      return;
     }
+
+    // Login user
+    const result = await signIn("credentials", {
+      redirect: true,
+      callbackUrl: "/",
+      email: values.email,
+      password: values.password,
+    });
+
+    if (result?.error) {
+      notifications.show({
+        title: "Login failed",
+        message: `We couldn't log you in. Please try again.`,
+        color: "red",
+      });
+      setLoading(false);
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    notifications.show({
+      title: "Registration failed",
+      message: `An unexpected error occurred. Please try again later.`,
+      color: "red",
+    });
+  } finally {
+    setLoading(false);
+  }
 };
