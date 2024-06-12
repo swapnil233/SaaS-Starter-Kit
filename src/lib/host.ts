36a4b7b370bb137de3a66346c387adb28774a26d
Hasan Iqbal <swapniliqbal@gmail.com>
2024-06-11 22:57:32 -0400

    style: npm run prettier

--- src/lib/host.ts
+++ src/lib/host.ts
@@ -1,3 +1,4 @@
-export const host = process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
+export const host =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
     : `https://${process.env.VERCEL_URL}`;
