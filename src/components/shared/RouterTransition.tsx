09cc59d76984146215aeaa76d40038d7d1d38ed2
Hasan Iqbal <swapniliqbal@gmail.com>
2024-06-11 22:55:43 -0400

    fix: only show router transition when route is changing. Also only show it at the top

--- src/components/shared/RouterTransition.tsx
+++ src/components/shared/RouterTransition.tsx
@@ -3,17 +3,25 @@ import { useMantineTheme } from "@mantine/core";
 import { useColorScheme } from "@mantine/hooks";
 import { NavigationProgress, nprogress } from "@mantine/nprogress";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function RouterTransition() {
   const router = useRouter();
   const theme = useMantineTheme();
   const colorScheme = useColorScheme();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const handleStart = (url: string) =>
-      url !== router.asPath && nprogress.start();
-    const handleComplete = () => nprogress.complete();
+    const handleStart = (url: string) => {
+      if (url !== router.asPath) {
+        setLoading(true);
+        nprogress.start();
+      }
+    };
+    const handleComplete = () => {
+      setLoading(false);
+      nprogress.complete();
+    };
 
     router.events.on("routeChangeStart", handleStart);
     router.events.on("routeChangeComplete", handleComplete);
@@ -27,9 +35,14 @@ export function RouterTransition() {
   }, [router.asPath, router.events]);
 
   return (
-    <NavigationProgress
-      color={colorScheme === "dark" ? "green" : theme.primaryColor}
-      size={5}
-    />
+    <>
+      {loading && (
+        <NavigationProgress
+          className="fixed top-0 left-0 w-full z-50"
+          color={colorScheme === "dark" ? "green" : theme.primaryColor}
+          size={5}
+        />
+      )}
+    </>
   );
 }
