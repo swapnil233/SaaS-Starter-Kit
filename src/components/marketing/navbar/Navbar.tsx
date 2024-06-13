43372aed5bc27de54523c0a754a4ecc4b654b46b
Hasan Iqbal <swapniliqbal@gmail.com>
2024-06-12 23:44:19 -0400

    fix: fix navbar links

--- src/components/marketing/navbar/Navbar.tsx
+++ src/components/marketing/navbar/Navbar.tsx
@@ -16,54 +16,15 @@ import {
   ScrollArea,
   rem,
   useMantineTheme,
+  Stack,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import {
-  IconNotification,
-  IconCode,
-  IconBook,
-  IconChartPie3,
-  IconFingerprint,
-  IconCoin,
-  IconChevronDown,
-} from "@tabler/icons-react";
+import { IconChevronDown } from "@tabler/icons-react";
 import classes from "./Navbar.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import app from "@/lib/app";
-
-const mockdata = [
-  {
-    icon: IconCode,
-    title: "Open source",
-    description: "This Pokémon’s cry is very loud and distracting",
-  },
-  {
-    icon: IconCoin,
-    title: "Free for everyone",
-    description: "The fluid of Smeargle’s tail secretions changes",
-  },
-  {
-    icon: IconBook,
-    title: "Documentation",
-    description: "Yanma is capable of seeing 360 degrees without",
-  },
-  {
-    icon: IconFingerprint,
-    title: "Security",
-    description: "The shell’s rounded shape and the grooves on its.",
-  },
-  {
-    icon: IconChartPie3,
-    title: "Analytics",
-    description: "This Pokémon uses its flying ability to quickly chase",
-  },
-  {
-    icon: IconNotification,
-    title: "Notifications",
-    description: "Combusken battles with the intensely hot flames it spews",
-  },
-];
+import { featuresList } from "@/lib/marketing/featuresList";
 
 export function Navbar() {
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
@@ -71,7 +32,8 @@ export function Navbar() {
   const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
   const theme = useMantineTheme();
 
-  const links = mockdata.map((item) => (
+  // Show the first 4 from the master list of features
+  const links = featuresList.slice(0, 4).map((item) => (
     <UnstyledButton className={classes.subLink} key={item.title}>
       <Group wrap="nowrap" align="flex-start">
         <ThemeIcon size={34} variant="default" radius="md">
@@ -133,7 +95,7 @@ export function Navbar() {
               <HoverCard.Dropdown style={{ overflow: "hidden" }}>
                 <Group justify="space-between" px="md">
                   <Text fw={500}>Features</Text>
-                  <Anchor href="#" fz="xs">
+                  <Anchor href="#features" fz="xs">
                     View all
                   </Anchor>
                 </Group>
@@ -189,40 +151,63 @@ export function Navbar() {
         onClose={closeDrawer}
         size="100%"
         padding="md"
-        title="Navigation"
+        title={
+          <Link href="/">
+            <Image
+              src={app.logoUrl}
+              alt={app.logoUrlAlt}
+              height={40}
+              width={40}
+            />
+          </Link>
+        }
         hiddenFrom="sm"
         zIndex={1000000}
       >
         <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
           <Divider my="sm" />
 
-          <a href="#" className={classes.link}>
-            Home
-          </a>
-          <UnstyledButton className={classes.link} onClick={toggleLinks}>
-            <Center inline>
-              <Box component="span" mr={5}>
-                Features
-              </Box>
-              <IconChevronDown
-                style={{ width: rem(16), height: rem(16) }}
-                color={theme.colors.blue[6]}
-              />
-            </Center>
-          </UnstyledButton>
-          <Collapse in={linksOpened}>{links}</Collapse>
-          <a href="#" className={classes.link}>
-            Learn
-          </a>
-          <a href="#" className={classes.link}>
-            Academy
-          </a>
+          <Stack gap={"xl"}>
+            <Link href="/" className={classes.link}>
+              Home
+            </Link>
+            <UnstyledButton className={classes.link} onClick={toggleLinks}>
+              <Center inline>
+                <Box component="span" mr={5}>
+                  Features
+                </Box>
+                <IconChevronDown
+                  style={{ width: rem(16), height: rem(16) }}
+                  color={theme.colors.blue[6]}
+                />
+              </Center>
+            </UnstyledButton>
+            <Collapse in={linksOpened}>{links}</Collapse>
+            <Link
+              href="#testimonials"
+              className={classes.link}
+              onClick={closeDrawer}
+            >
+              Testimonials
+            </Link>
+            <Link
+              href="#pricing"
+              className={classes.link}
+              onClick={closeDrawer}
+            >
+              Pricing
+            </Link>
+          </Stack>
 
           <Divider my="sm" />
 
           <Group justify="center" grow pb="xl" px="md">
-            <Button variant="default">Log in</Button>
-            <Button>Sign up</Button>
+            <Button variant="default" component={Link} href="/signin">
+              Sign in
+            </Button>
+            <Button component={Link} href="/register">
+              Register
+            </Button>
           </Group>
         </ScrollArea>
       </Drawer>
