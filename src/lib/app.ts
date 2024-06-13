cc407d8217716b15e45014ab64cc563ef91cacf1
Hasan Iqbal <swapniliqbal@gmail.com>
2024-06-13 00:59:07 -0400

    style: npm run prettier

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -4,7 +4,8 @@ const app = {
   version: packageInfo.version,
   author: "Hasan Iqbal",
   name: "Boilerplate",
-  description: "Boilerplate is a SaaS starter-kit built with Next.js pages and MantineUI.",
+  description:
+    "Boilerplate is a SaaS starter-kit built with Next.js pages and MantineUI.",
   logoUrl: "/logo.svg",
   logoUrlAlt: "Boilerplate logo",
 };
