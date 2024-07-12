4a304ab2a0498b7e751fe475e9baad71c880b440
Hasan Iqbal <swapniliqbal@gmail.com>
2024-07-12 12:19:51 -0400

    feat: implement designs for the reset password page when the link is expired or does not exist

---
+++ src/components/auth/ExpiredPasswordResetLink.tsx
@@ -0,0 +1,43 @@
+import app from "@/lib/app";
+import { Button, Paper, Stack, Text, Title } from "@mantine/core";
+import Image from "next/image";
+import Link from "next/link";
+
+export interface IExpiredPasswordResetLinkProps {}
+
+const ExpiredPasswordResetLink: React.FC<
+  IExpiredPasswordResetLinkProps
+> = () => {
+  return (
+    <Paper radius="md" p="md" m={"lg"} w={"95%"} maw={450}>
+      <Stack justify="stretch" gap="xs" mb="md" align="center">
+        {app.logoUrl && (
+          <Link href="/">
+            <Image
+              src={app.logoUrl}
+              alt={app.logoUrlAlt}
+              height={60}
+              width={60}
+            />
+          </Link>
+        )}
+        <Stack align="center" mt={"md"} gap={4}>
+          <Title
+            order={3}
+            style={{
+              textAlign: "center",
+            }}
+          >
+            This link is invalid or expired
+          </Title>
+          <Text>Please login to continue.</Text>
+        </Stack>
+        <Button component={Link} href="/signin">
+          Sign in
+        </Button>
+      </Stack>
+    </Paper>
+  );
+};
+
+export default ExpiredPasswordResetLink;
--- src/pages/reset-password.tsx
+++ src/pages/reset-password.tsx
@@ -1,7 +1,8 @@
+import ExpiredPasswordResetLink from "@/components/auth/ExpiredPasswordResetLink";
 import { ResetPasswordForm } from "@/components/auth/ResetPasswordForm";
 import SharedHead from "@/components/shared/SharedHead";
 import app from "@/lib/app";
-import { Stack, Text } from "@mantine/core";
+import { Stack } from "@mantine/core";
 import { useRouter } from "next/router";
 import { FC } from "react";
 
@@ -9,18 +10,18 @@ const ResetPasswordPage: FC = () => {
   const router = useRouter();
   const { token } = router.query;
 
-  if (!token) {
-    return <Text>Error: Token is missing or invalid.</Text>;
-  }
-
   return (
     <>
       <SharedHead
         title="Reset password"
-        description={`Sign into ${app.name}`}
+        description={`Reset your password for ${app.name}`}
       />
       <Stack justify="center" align="center">
-        <ResetPasswordForm token={token as string} />
+        {!token ? (
+          <ExpiredPasswordResetLink />
+        ) : (
+          <ResetPasswordForm token={token as string} />
+        )}
       </Stack>
     </>
   );
