ea52193a33ccdf3df27f8b9d3e59e065aecaa47e
Hasan Iqbal <swapniliqbal@gmail.com>
2024-07-13 17:02:55 -0400

    refactor: use Tailwind for hero section styles instead of module css

--- src/components/marketing/hero/Hero.tsx
+++ src/components/marketing/hero/Hero.tsx
@@ -1,49 +1,43 @@
-import { Container, Title, Text, Button } from "@mantine/core";
-import classes from "./Hero.module.css";
+import { Button, Text, Title } from "@mantine/core";
 import { IconBrandGithub } from "@tabler/icons-react";
 import Link from "next/link";
 
 export function Hero() {
   return (
-    <div className={classes.root}>
-      <Container size="lg">
-        <div className={classes.inner}>
-          <div className={classes.content}>
-            <Title className={classes.title}>
-              A{" "}
-              <Text
-                component="span"
-                inherit
-                variant="gradient"
-                gradient={{ from: "pink", to: "yellow" }}
-              >
-                fully featured
-              </Text>{" "}
-              Next.js SaaS Starter Kit
-            </Title>
+    <div className="py-20 md:py-40 px-4 bg-[#11284b] bg-cover bg-center bg-[linear-gradient(250deg,rgba(130,201,30,0)_0%,#062343_70%),url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8&auto=format&fit=crop&w=1080&q=80')]">
+      <div className="container mx-auto max-w-[1100px] ">
+        <Title className="text-white font-bold leading-[1.05] max-w-[500px] text-[48px] md:max-w-full md:text-[34px] md:leading-[1.15]">
+          A{" "}
+          <Text
+            component="span"
+            inherit
+            variant="gradient"
+            gradient={{ from: "pink", to: "yellow" }}
+          >
+            fully featured
+          </Text>{" "}
+          Next.js SaaS Starter Kit
+        </Title>
 
-            <Text className={classes.description} mt={30}>
-              Ship faster with Boilerplate - a Next.js (pages router) SaaS
-              starter kit built to speed up development of B2C SaaS products.
-              Features include a full authentication system, Stripe
-              subscriptions, etc. Built with Mantine UI components.{" "}
-            </Text>
+        <Text className="text-white opacity-75 max-w-[500px] mt-7 md:max-w-full">
+          Ship faster with Boilerplate - a Next.js (pages router) SaaS starter
+          kit built to speed up development of B2C SaaS products. Features
+          include a full authentication system, Stripe subscriptions, etc. Built
+          with Mantine UI components.{" "}
+        </Text>
 
-            <Button
-              size="lg"
-              className={classes.control}
-              mt={40}
-              leftSection={<IconBrandGithub size={"1.4rem"} />}
-              component={Link}
-              href={"https://github.com/swapnil233/boilerplate"}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              View repository
-            </Button>
-          </div>
-        </div>
-      </Container>
+        <Button
+          size="lg"
+          leftSection={<IconBrandGithub size={"1.4rem"} />}
+          component={Link}
+          href={"https://github.com/swapnil233/boilerplate"}
+          rel="noopener noreferrer"
+          target="_blank"
+          className="mt-10"
+        >
+          View repository
+        </Button>
+      </div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { NextPageWithLayout } from "./page";
-import HomePageLayout from "@/components/shared/layouts/HomePageLayout";
-import { Box } from "@mantine/core";
-import { Navbar } from "@/components/marketing/navbar/Navbar";
-import { Hero } from "@/components/marketing/hero/Hero";
 import { FeaturesGrid } from "@/components/marketing/features/FeaturesGrid";
 import { Footer } from "@/components/marketing/footer/Footer";
-import SharedHead from "@/components/shared/SharedHead";
+import { Hero } from "@/components/marketing/hero/Hero";
+import { Navbar } from "@/components/marketing/navbar/Navbar";
 import PricingSection from "@/components/marketing/pricing/PricingSection";
+import HomePageLayout from "@/components/shared/layouts/HomePageLayout";
+import SharedHead from "@/components/shared/SharedHead";
+import { Box } from "@mantine/core";
+import { NextPageWithLayout } from "./page";
 
 const Home: NextPageWithLayout = () => {
   return (
