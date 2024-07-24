542dad386cda4cc927529b62b314c1ec3dd08947
Hasan Iqbal <swapniliqbal@gmail.com>
2024-07-24 01:45:22 -0400

    feat: implement collapsible sidenav on dashboard

--- src/components/buttons/NavButton.tsx
+++ src/components/buttons/NavButton.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from "@mantine/core";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { FC, ReactNode } from "react";
@@ -6,6 +7,7 @@ interface NavButtonProps {
   href: string;
   icon: ReactNode;
   label: string;
+  isCollapsed: boolean;
   closeMobileNav: () => void;
 }
 
@@ -13,6 +15,7 @@ const NavButton: FC<NavButtonProps> = ({
   href,
   icon,
   label,
+  isCollapsed,
   closeMobileNav,
 }) => {
   const router = useRouter();
@@ -24,19 +27,25 @@ const NavButton: FC<NavButtonProps> = ({
     router.push(href);
   };
 
-  return (
+  const linkContent = (
     <Link
       href={href}
       onClick={handleClick}
-      className={`flex items-center px-4 py-3 text-sm rounded-md ${
+      className={`flex ${isCollapsed ? "px-2" : "px-[13px]"} py-3 text-sm rounded-md ${
         isActive
           ? "bg-gray-200 text-gray-900 font-semibold"
           : "text-gray-600 hover:bg-gray-100"
-      }`}
+      } ${isCollapsed && "justify-center"}`}
     >
-      <span className="mr-3">{icon}</span>
-      {label}
+      <span className={isCollapsed ? "" : "mr-3"}>{icon}</span>
+      {!isCollapsed && <span>{label}</span>}
     </Link>
+  );
+
+  return isCollapsed ? (
+    <Tooltip label={label}>{linkContent}</Tooltip>
+  ) : (
+    linkContent
   );
 };
 
--- src/components/shared/layouts/DashboardLayout.tsx
+++ src/components/shared/layouts/DashboardLayout.tsx
@@ -1,10 +1,12 @@
 import NavButton from "@/components/buttons/NavButton";
+import app from "@/lib/app";
 import {
   AppShell,
   Burger,
   Group,
   ScrollArea,
   Stack,
+  Text,
   TextInput,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
@@ -15,11 +17,14 @@ import {
   IconFile,
   IconHome,
   IconInbox,
+  IconLayoutSidebarLeftCollapse,
+  IconLayoutSidebarLeftExpand,
   IconSearch,
   IconUser,
-  IconUserPlus,
 } from "@tabler/icons-react";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
 import { FC, ReactNode, useState } from "react";
 import NotificationsButton from "../user/NotificationsButton";
 import UserNavMenu from "../user/UserNavMenu";
@@ -35,31 +40,45 @@ const DashboardLayout: FC<IDashboardLayout> = ({ children }) => {
   const [newNotifications] = useState(false);
   const session = useSession();
 
+  const handleCollapseClick = () => {
+    toggleDesktop();
+    closeMobile();
+  };
+
   return (
     <AppShell
       header={{ height: 70 }}
       navbar={{
-        width: 250,
+        width: { base: 300, sm: desktopOpened ? 250 : 80 },
         breakpoint: "sm",
-        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+        collapsed: { mobile: !mobileOpened, desktop: false },
       }}
       padding="md"
     >
       <AppShell.Header>
-        <Group h="100%" px="md" justify="space-between">
+        <Group h="100%" px="md" justify="space-between" wrap="nowrap">
           <Burger
             opened={mobileOpened}
             onClick={toggleMobile}
             hiddenFrom="sm"
             size="sm"
           />
-          <Burger
-            opened={desktopOpened}
-            onClick={toggleDesktop}
-            visibleFrom="sm"
-            size="sm"
-          />
-          <Group gap="xs">
+          {app.logoUrl && (
+            <Link href="/">
+              <Group align="center" wrap="nowrap" gap="sm">
+                <Image
+                  src={app.logoUrl}
+                  alt={app.logoUrlAlt}
+                  height={38}
+                  width={38}
+                />
+                <Text size="xl" fw={700} visibleFrom="sm">
+                  {app.name}
+                </Text>
+              </Group>
+            </Link>
+          )}
+          <Group gap="xs" justify="flex-end" w="100%">
             <TextInput
               leftSection={<IconSearch size="1.1rem" />}
               placeholder="Search and discover"
@@ -75,76 +94,90 @@ const DashboardLayout: FC<IDashboardLayout> = ({ children }) => {
           </Group>
         </Group>
       </AppShell.Header>
-      <AppShell.Navbar>
-        <AppShell.Section p="md" grow component={ScrollArea}>
-          <Stack>
-            <Stack>
-              <Stack gap="xs">
-                <NavButton
-                  href="/dashboard"
-                  icon={<IconHome size={20} />}
-                  label="Home"
-                  closeMobileNav={closeMobile}
-                />
-                <NavButton
-                  href="/inbox"
-                  icon={<IconInbox size={20} />}
-                  label="Inbox"
-                  closeMobileNav={closeMobile}
-                />
-                <NavButton
-                  href="/reports"
-                  icon={<IconChartBar size={20} />}
-                  label="Reports"
-                  closeMobileNav={closeMobile}
-                />
-              </Stack>
 
-              <Stack gap="xs">
+      <AppShell.Navbar
+        w={{ base: 300, sm: desktopOpened ? 250 : 80 }}
+        style={{ zIndex: 200 }}
+      >
+        <AppShell.Section px="md" pt="md" pb={0} grow component={ScrollArea}>
+          <Stack h="100%">
+            <Stack gap="xs">
+              <NavButton
+                href="/dashboard"
+                icon={<IconHome size={20} />}
+                label="Home"
+                isCollapsed={!desktopOpened}
+                closeMobileNav={closeMobile}
+              />
+              <NavButton
+                href="/inbox"
+                icon={<IconInbox size={20} />}
+                label="Inbox"
+                isCollapsed={!desktopOpened}
+                closeMobileNav={closeMobile}
+              />
+              <NavButton
+                href="/reports"
+                icon={<IconChartBar size={20} />}
+                label="Reports"
+                isCollapsed={!desktopOpened}
+                closeMobileNav={closeMobile}
+              />
+              {desktopOpened && (
                 <div className="mt-4 px-4 text-xs font-semibold text-gray-400 uppercase">
                   Apps
                 </div>
-                <NavButton
-                  href="/documents"
-                  icon={<IconFile size={20} />}
-                  label="Documents"
-                  closeMobileNav={closeMobile}
-                />
-                <NavButton
-                  href="/automations"
-                  icon={<IconBolt size={20} />}
-                  label="Automations"
-                  closeMobileNav={closeMobile}
-                />
-              </Stack>
-
-              <Stack gap="xs">
+              )}
+              <NavButton
+                href="/documents"
+                icon={<IconFile size={20} />}
+                label="Documents"
+                isCollapsed={!desktopOpened}
+                closeMobileNav={closeMobile}
+              />
+              <NavButton
+                href="/automations"
+                icon={<IconBolt size={20} />}
+                label="Automations"
+                isCollapsed={!desktopOpened}
+                closeMobileNav={closeMobile}
+              />
+              {desktopOpened && (
                 <div className="mt-4 px-4 text-xs font-semibold text-gray-400 uppercase">
                   Settings
                 </div>
-                <NavButton
-                  href="/dashboard/account"
-                  icon={<IconUser size={20} />}
-                  label="Account"
-                  closeMobileNav={closeMobile}
-                />
-                <NavButton
-                  href="/dashboard/plans"
-                  icon={<IconBuildingBank size={20} />}
-                  label="Pricing plans"
-                  closeMobileNav={closeMobile}
-                />
-              </Stack>
-            </Stack>
-            <div className="absolute bottom-0 left-0 p-4 w-full border-t">
+              )}
               <NavButton
-                href="/invite"
-                icon={<IconUserPlus size={20} />}
-                label="Invite people"
+                href="/dashboard/account"
+                icon={<IconUser size={20} />}
+                label="Account"
+                isCollapsed={!desktopOpened}
                 closeMobileNav={closeMobile}
               />
-            </div>
+              <NavButton
+                href="/dashboard/plans"
+                icon={<IconBuildingBank size={20} />}
+                label="Pricing plans"
+                isCollapsed={!desktopOpened}
+                closeMobileNav={closeMobile}
+              />
+            </Stack>
           </Stack>
+        </AppShell.Section>
+        <AppShell.Section px="md" py={"xs"} visibleFrom="sm">
+          <NavButton
+            href="#"
+            icon={
+              desktopOpened ? (
+                <IconLayoutSidebarLeftCollapse size={20} />
+              ) : (
+                <IconLayoutSidebarLeftExpand size={20} />
+              )
+            }
+            label="Collapse"
+            isCollapsed={!desktopOpened}
+            closeMobileNav={handleCollapseClick}
+          />
         </AppShell.Section>
       </AppShell.Navbar>
       <AppShell.Main bg="#f7fafd">{children}</AppShell.Main>
